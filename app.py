aaaabe781ad14991e5dce5a6e4a951f297269333
DavidJohn <super.star31305@gmail.com>
2023-02-28 08:43:25 -0500

    Set wide mode.

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import streamlit as st
 from snowiq import st_custom_slider
 
 v_custom = st_custom_slider('Hello world', 0, 100, '', key="slider1")
+st.set_page_config(layout="wide")
 st.write(v_custom)
